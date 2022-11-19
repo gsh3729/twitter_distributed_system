2864e7b477d6dbcd3ff99d5baecc0d1fb327c227
anubis-robot <anubis@anubis-lms.io>
2022-11-19 20:38:13 +0000

    Anubis Cloud IDE Autosave netid=sg7372

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	//"html/template"
+	//"strings"
+
+	globals "gin_session_auth/globals"
+	middleware "gin_session_auth/middleware"
+	routes "gin_session_auth/routes"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Static("/assets", "./assets")
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.Use(sessions.Sessions("session", cookie.NewStore(globals.Secret)))
+
+	public := router.Group("/")
+	routes.PublicRoutes(public)
+
+	private := router.Group("/")
+	private.Use(middleware.AuthRequired)
+	routes.PrivateRoutes(private)
+
+	router.Run("localhost:8080")
+}
