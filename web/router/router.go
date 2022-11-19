b6e289497f8b407679d3466b6e67a089b623a551
anubis-robot <anubis@anubis-lms.io>
2022-11-19 17:51:22 +0000

    Anubis Cloud IDE Autosave netid=sg7372

---
+++ web/router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	// "go-server/middleware"
+	"github.com/gorilla/mux"
+)
+
+// Router is exported and used in main.go
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/login", middleware.GetAllTask).Methods("GET", "OPTIONS")
+	router.HandleFunc("/signup", middleware.CreateTask).Methods("POST", "OPTIONS")
+	router.HandleFunc("/profile/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/compose/tweet/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/connect/people/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
+	return router
+}
