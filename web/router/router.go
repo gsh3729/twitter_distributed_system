d15a40df5f2794daf2a9b9c581767125ef909a7c
anubis-robot <anubis@anubis-lms.io>
2022-11-19 20:48:14 +0000

    Anubis Cloud IDE Autosave netid=sg7372

--- web/router/router.go
+++ web/router/router.go
@@ -1,20 +1,43 @@
-package router
+// package router
+
+// import (
+// 	// "go-server/middleware"
+// 	"github.com/gorilla/mux"
+// )
+
+// // Router is exported and used in main.go
+// func Router() *mux.Router {
+
+// 	router := mux.NewRouter()
+
+// 	router.HandleFunc("/login", middleware.GetAllTask).Methods("GET", "OPTIONS")
+// 	router.HandleFunc("/signup", middleware.CreateTask).Methods("POST", "OPTIONS")
+// 	router.HandleFunc("/profile/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
+// 	router.HandleFunc("/compose/tweet/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
+// 	router.HandleFunc("/connect/people/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
+// 	// router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
+// 	return router
+// }
+
+package routes
 
 import (
-	// "go-server/middleware"
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+
+	controllers "web/controllers"
 )
 
-// Router is exported and used in main.go
-func Router() *mux.Router {
+func PublicRoutes(g *gin.RouterGroup) {
 
-	router := mux.NewRouter()
+	g.GET("/login", controllers.LoginGetHandler())
+	g.POST("/login", controllers.LoginPostHandler())
+	g.GET("/", controllers.IndexGetHandler())
 
-	router.HandleFunc("/login", middleware.GetAllTask).Methods("GET", "OPTIONS")
-	router.HandleFunc("/signup", middleware.CreateTask).Methods("POST", "OPTIONS")
-	router.HandleFunc("/profile/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/compose/tweet/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/connect/people/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
-	// router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
-	return router
+}
+
+func PrivateRoutes(g *gin.RouterGroup) {
+
+	g.GET("/dashboard", controllers.DashboardGetHandler())
+	g.GET("/logout", controllers.LogoutGetHandler())
+
 }
