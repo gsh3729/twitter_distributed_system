16597f102c6e7a64bd5d310a0123f1ca882881f9
anubis-robot <anubis@anubis-lms.io>
2022-11-22 02:49:54 +0000

    Anubis Cloud IDE Autosave netid=sg7372

--- web/tests/tweet_test.go
+++ web/tests/tweet_test.go
@@ -11,7 +11,23 @@ import (
 
 func TestCart(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Shopping Cart Suite")
+	RunSpecs(t, "Posting a tweet")
 }
 
-var _ = Describe("Shopping cart", func() {
+var _ = Describe("Tweet", func() {
+	Context("initially", func() {
+		cart := Cart{}
+
+		It("has 0 items", func() {
+			Expect(cart.TotalUniqueItems()).Should(BeZero())
+		})
+
+		It("has 0 units", func() {
+			Expect(cart.TotalUnits()).Should(BeZero())
+		})
+
+		Specify("the total amount is 0.00", func() {
+			Expect(cart.TotalAmount()).Should(BeZero())
+		})
+	})
+})
