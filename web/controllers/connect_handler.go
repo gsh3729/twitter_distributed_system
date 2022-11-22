66995fb6c3b6369d834dee9f46d92b73b2faecd7
anubis-robot <anubis@anubis-lms.io>
2022-11-22 02:39:52 +0000

    Anubis Cloud IDE Autosave netid=sg7372

--- web/controllers/connect_handler.go
+++ web/controllers/connect_handler.go
@@ -11,29 +11,7 @@ import (
 	"log"
 )
 
-// func ConnectPostHandler() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		session := sessions.Default(c)
-// 		user := session.Get(globals.Userkey)
-// 		if user != nil {
-// 			c.Redirect(http.StatusAccepted, "/dashboard")
-// 			return
-// 		}
 
-// 		username := c.Request.URL.Path[len("/connect/"):]
-// 		connectTo := c.PostForm("connectTo")
-// 		userFollowing := globals.Following[username]
-// 		userFollowing = append(userFollowing, connectTo)
-
-// 		userFollowers := globals.Followers[connectTo]
-// 		userFollowers = append(userFollowers, username)
-
-// 		// c.HTML(http.StatusOK, "followers.html", gin.H{
-// 		// 	"content": userFollowers,
-// 		// 	"user":    user,
-// 		// })
-// 	}
-// } 
 
 func ConnectPostHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
