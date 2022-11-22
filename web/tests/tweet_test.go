1f09cb3761e3db3c48de6e62277d386166c4f02e
anubis-robot <anubis@anubis-lms.io>
2022-11-22 02:54:54 +0000

    Anubis Cloud IDE Autosave netid=sg7372

--- web/tests/tweet_test.go
+++ web/tests/tweet_test.go
@@ -30,4 +30,29 @@ var _ = Describe("Tweet", func() {
 			Expect(cart.TotalAmount()).Should(BeZero())
 		})
 	})
+
+	Context("when a new item is added", func() {
+		cart := Cart{}
+
+		originalItemCount := cart.TotalUniqueItems()
+		originalUnitCount := cart.TotalUnits()
+		originalAmount := cart.TotalAmount()
+
+		cart.AddItem(itemA)
+
+		Context("the shopping cart", func() {
+			It("has 1 more unique item than it had earlier", func() {
+				Expect(cart.TotalUniqueItems()).Should(Equal(originalItemCount + 1))
+			})
+
+			It("has 1 more unit than it had earlier", func() {
+				Expect(cart.TotalUnits()).Should(Equal(originalUnitCount + 1))
+			})
+
+			Specify("total amount increases by item price", func() {
+				Expect(cart.TotalAmount()).Should(Equal(originalAmount + itemA.Price))
+			})
+		})
+	})
+
 })
