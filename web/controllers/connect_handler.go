6e75afb8ba4fce2cfcff29db213b55c60f7452d4
anubis-robot <anubis@anubis-lms.io>
2022-11-28 00:28:41 +0000

    Anubis Cloud IDE Autosave netid=ts4044

--- web/controllers/connect_handler.go
+++ web/controllers/connect_handler.go
@@ -26,3 +26,21 @@ func ConnectPostHandler() gin.HandlerFunc {
 		c.Redirect(http.StatusMovedPermanently, "/dashboard")
 	}
 }
+
+func UnfollowPostHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := session.Get(globals.Userkey)
+		username := user.(string)
+
+		unfollowPerson := c.PostForm("unfollowPerson")
+
+		i := helpers.IndexOf(unfollowPerson, globals.Following[username])
+		globals.Following[username] = helpers.RemoveFromSlice(globals.Following[username], i)
+
+		j := helpers.IndexOf(username, globals.Followers[unfollowPerson])
+		globals.Followers[unfollowPerson] = helpers.RemoveFromSlice(globals.Followers[unfollowPerson], j)
+
+		c.Redirect(http.StatusMovedPermanently, "/dashboard")
+	}
+}
--- web/controllers/following_handler.go
+++ web/controllers/following_handler.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 
 	globals "proj/web/globals"
-	helpers "proj/web/helpers"
 )
 
 func FollowingGetHandler() gin.HandlerFunc {
@@ -23,23 +22,5 @@ func FollowingGetHandler() gin.HandlerFunc {
 			"content": userFollowers,
 			"user":    user,
 		})
-	}
-}
-
-func UnfollowPostHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		user := session.Get(globals.Userkey)
-		username := user.(string)
-
-		unfollowPerson := c.PostForm("unfollowPerson")
-
-		i := helpers.IndexOf(unfollowPerson, globals.Following[username])
-		globals.Following[username] = helpers.RemoveFromSlice(globals.Following[username], i)
-
-		j := helpers.IndexOf(username, globals.Followers[unfollowPerson])
-		globals.Followers[unfollowPerson] = helpers.RemoveFromSlice(globals.Followers[unfollowPerson], j)
-
-		c.Redirect(http.StatusMovedPermanently, "/dashboard")
 	}
 }
