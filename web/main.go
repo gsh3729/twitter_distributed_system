d131535ea174f0a164f86e36bae438c993fb195d
anubis-robot <anubis@anubis-lms.io>
2022-12-11 15:47:41 +0000

    Anubis Cloud IDE Autosave netid=ts4044

---
+++ web/main.go
@@ -0,0 +1,25 @@
+package web
+
+import (
+	middleware "proj/web/auth"
+	globals "proj/web/globals"
+	routes "proj/web/router"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
