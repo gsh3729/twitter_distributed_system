603f0515a2e1a7b5ea5f99347af35de5eab2b9b4
anubis-robot <anubis@anubis-lms.io>
2022-12-14 22:27:21 +0000

    Anubis Cloud IDE Autosave netid=sg7372

--- backend/auth/auth_service.go
+++ backend/auth/auth_service.go
@@ -1,10 +1,30 @@
 package auth
 
 import (
+	"context"
 	"log"
 	globals "backend/globals"
 	// helpers "backend/helpers"
 )
+
+func (r rpcInterface) AddWord(ctx context.Context, req *pb.AddWordRequest) (*pb.AddWordResponse, error) {
+	f := r.raft.Apply([]byte(req.GetWord()), time.Second)
+	if err := f.Error(); err != nil {
+		return nil, rafterrors.MarkRetriable(err)
+	}
+	return &pb.AddWordResponse{
+		CommitIndex: f.Index(),
+	}, nil
+}
+
+func (r rpcInterface) GetWords(ctx context.Context, req *pb.GetWordsRequest) (*pb.GetWordsResponse, error) {
+	r.wordTracker.mtx.RLock()
+	defer r.wordTracker.mtx.RUnlock()
+	return &pb.GetWordsResponse{
+		BestWords:   cloneWords(r.wordTracker.words),
+		ReadAtIndex: r.raft.AppliedIndex(),
+	}, nil
+}
 
 func SignUp(username string, password string) string {
 	globals.UserPass[username] = password
