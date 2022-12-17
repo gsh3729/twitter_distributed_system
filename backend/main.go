09c59720896abc58ec9f6a37e975e0685d0409c2
anubis-robot <anubis@anubis-lms.io>
2022-12-17 23:19:40 +0000

    Anubis Cloud IDE Autosave netid=ts4044

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,8 @@ import (
 	"net"
 
 	"backend/authbackend"
+	"backend/follow"
+	"backend/tweet"
 
 	"google.golang.org/grpc"
 )
@@ -17,10 +19,14 @@ func main() {
 	}
 
 	auth_server := authbackend.Server{}
+	follow_server := follow.Server{}
+	tweet_server := tweet.Server{}
 
 	Server := grpc.NewServer()
 
 	authbackend.RegisterAuthServiceServer(Server, &auth_server)
+	follow.RegisterFollowServiceServer(Server, &follow_server)
+	tweet.RegisterTweetServiceServer(Server, &tweet_server)
 
 	if err := Server.Serve(lis); err != nil {
 		log.Fatalf("Failed to Listen to TCP: %s", err)
