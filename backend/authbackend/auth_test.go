64cb1cfd682ee55824adb9dfd8a6a8b6a2d632c3
anubis-robot <anubis@anubis-lms.io>
2022-12-19 02:56:19 +0000

    Anubis Cloud IDE Autosave netid=sg7372

--- backend/authbackend/auth_test.go
+++ backend/authbackend/auth_test.go
@@ -25,12 +25,8 @@ func TestAuth1(t *testing.T) {
 		Password: password,
 	})
 
-	if err != nil {
-		t.Fatalf("TestAuth signup failed: %v", err)
-	}
-
-	if !response.Success {
-		t.Error("TestAuth signup Failed")
+	if err != nil || !response.Success {
+		t.Error("TestAuth signup failed: ", err)
 	}
 
 	resp, err := auth_server.SignIn(context.Background(), &UserSignInRequest{
@@ -38,13 +34,43 @@ func TestAuth1(t *testing.T) {
 		Password: password,
 	})
 
-	if err != nil {
-		t.Fatalf("TestAuth signin failed: %v", err)
+	if err != nil || !resp.Success {
+		t.Error("TestAuth signin failed: ", err)
 	}
 
-	if !resp.Success {
-		t.Error("TestAuth signin Failed")
+	log.Printf("Auth test1 passed successfully")
+}
+
+
+func TestAuth2(t *testing.T) {
+	var conn *grpc.ClientConn
+	conn, err2 := grpc.Dial(":9000", grpc.WithInsecure())
+	if err2 != nil {
+		log.Fatalf("Couldn't connect: %s", err2)
+	}
+	defer conn.Close()
+
+	var username string = "harshaG"
+	var password string = "proj123"
+
+	auth_server := NewAuthServiceClient(conn)
+	response, err := auth_server.SignUp(context.Background(), &UserSignUpRequest{
+		Username: username,
+		Password: password,
+	})
+
+	if err != nil || response.Success {
+		t.Error("TestAuth signup failed: ", err)
 	}
 
-	log.Printf("Auth tests passed successfully")
+	resp, err := auth_server.SignIn(context.Background(), &UserSignInRequest{
+		Username: username,
+		Password: password,
+	})
+
+	if err != nil || !resp.Success {
+		t.Error("TestAuth signin failed: ", err)
+	}
+
+	log.Printf("Auth test2 passed successfully")
 }
